bf9fb5bb75eba9f8910bfd2c55eb9a7fdcf2821e
msyk <nii@msyk.net>
2022-04-25 08:03:19 +0900

    Docker support started.

--- lib/params.php
+++ lib/params.php
@@ -34,7 +34,8 @@ $dbProtocol = 'HTTP';
 
 /* PDO awares below:
  */
-$dbDSN = 'mysql:host=127.0.0.1;dbname=test_db;charset=utf8mb4';
+$dbDSN = 'mysql:host=mysql;dbname=test_db;charset=utf8mb4';
+//$dbDSN = 'mysql:host=127.0.0.1;dbname=test_db;charset=utf8mb4';
 //$dbDSN = 'mysql:unix_socket=/tmp/mysql.sock;dbname=test_db;charset=utf8mb4';
 $dbOption = array();
 
