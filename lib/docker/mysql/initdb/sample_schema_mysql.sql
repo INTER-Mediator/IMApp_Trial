8c21405bdff72b2710de1ce9f454dd68bd23e009
msyk <nii@msyk.net>
2022-09-18 16:19:29 +0900

    Update MySQL schema.

--- lib/docker/mysql/initdb/sample_schema_mysql.sql
+++ lib/docker/mysql/initdb/sample_schema_mysql.sql
@@ -1170,9 +1170,8 @@ SELECT item.id,
        item.category_id,
        product.name,
        item.qty,
-       item.unitprice,
-       product.unitprice                                                         as unitprice_master,
-       IF(item.unitprice is null, qty * product.unitprice, qty * item.unitprice) AS amount
+       product.unitprice       as unitprice,
+       qty * product.unitprice AS amount
 FROM item,
      product
 WHERE item.product_id = product.id;
